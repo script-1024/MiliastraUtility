b534778a7a918704e080a9ce15f687c8592671e9
script-1024 <benny20050218@gmail.com>
2025-12-14 20:14:13 +0800

    改进反序列化方法，使其支持更灵活的类型

--- MiliastraUtility.Core/Serialization/ProtoFieldAttribute.cs
+++ MiliastraUtility.Core/Serialization/ProtoFieldAttribute.cs
@@ -1,34 +1,121 @@
 namespace MiliastraUtility.Core.Serialization;
 
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+/// <summary>
+/// 指示被修饰属性成为消息字段
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
 public class ProtoFieldAttribute : Attribute
 {
-
-    public required int Id
+    /// <summary>
+    /// 初始化一个新的 <see cref="ProtoFieldAttribute"/> 实例
+    /// </summary>
+    /// <param name="id">字段编号</param>
+    /// <param name="kind">
+    /// 字段类型。若此值被设为 <see cref="ValueKind.Wrapped"/>，还必须使用 <see cref="WrappedFieldAttribute"/> 修饰该属性。
+    /// </param>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public ProtoFieldAttribute(int id, ValueKind kind)
     {
-        get => field;
-        set
-        {
-            if (value < 1 || value > 65535)
-                throw new ArgumentOutOfRangeException(nameof(value), "无效的字段编号");
-            field = value;
-        }
+        if (id < 1 || id > 65535)
+            throw new ArgumentOutOfRangeException(nameof(id), "无效的字段编号，接受范围：[1, 65535]");
+        Id = id;
+        Kind = kind;
     }
 
-    public required ValueKind Kind { get; set; }
+    /// <summary>
+    /// 字段编号
+    /// </summary>
+    public int Id { get; }
 
-    public int WrapperId { get; set; } = 0;
+    /// <summary>
+    /// 字段类型
+    /// </summary>
+    /// <remarks>
+    /// 若此值被设为 <see cref="ValueKind.Wrapped"/>，还必须使用 <see cref="WrappedFieldAttribute"/> 修饰该属性。
+    /// </remarks>
+    public ValueKind Kind { get; }
+}
 
-    public bool Repeated { get; set; } = false;
+/// <summary>
+/// 指示被修饰属性成为被包装的消息字段
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class WrappedFieldAttribute : Attribute
+{
+    /// <summary>
+    /// 初始化一个新的 <see cref="WrappedFieldAttribute"/> 实例
+    /// </summary>
+    /// <param name="id">字段编号</param>
+    /// <param name="kind">字段类型，不接受 <see cref="ValueKind.Wrapped"/></param>
+    /// <param name="levels">每层包装的编号</param>
+    /// <exception cref="ArgumentException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public WrappedFieldAttribute(int id, ValueKind kind, params int[] levels)
+    {
+        if (id < 1 || id > 65535)
+            throw new ArgumentOutOfRangeException(nameof(id), "无效的字段编号，接受范围：[1, 65535]");
+        if (kind == ValueKind.Wrapped)
+            throw new ArgumentException("不接受 ValueKind.Wrapped 类型");
+        Id = id;
+        Kind = kind;
+        Levels = levels;
+    }
+
+    /// <summary>
+    /// 字段编号
+    /// </summary>
+    public int Id { get; }
+
+    /// <summary>
+    /// 字段类型
+    /// </summary>
+    public ValueKind Kind { get; }
+
+    /// <summary>
+    /// 每层包装的编号
+    /// </summary>
+    public int[] Levels { get; }
 }
 
 public enum ValueKind
 {
-    Varint = 0,
-    Fixed32 = 1,
-    Fixed64 = 2,
-    String = 3,
-    Object = 4,
-    List = 5,
-    Null = 6
+    /// <summary>
+    /// 表示一个内容总是为空的消息字段，该属性在反序列化时将被跳过
+    /// </summary>
+    Null = 0,
+
+    /// <summary>
+    /// 表示一个可变长度整数
+    /// </summary>
+    Varint = 1,
+
+    /// <summary>
+    /// 表示一个固定 32 位长的数值
+    /// </summary>
+    Fixed32 = 2,
+
+    /// <summary>
+    /// 表示一个固定 64 位长的数值
+    /// </summary>
+    Fixed64 = 3,
+
+    /// <summary>
+    /// 表示一个字符串
+    /// </summary>
+    String = 4,
+
+    /// <summary>
+    /// 表示一个对象
+    /// </summary>
+    Object = 5,
+
+    /// <summary>
+    /// 表示一个列表
+    /// </summary>
+    List = 6,
+
+    /// <summary>
+    /// 表示一个被包装的消息字段
+    /// </summary>
+    Wrapped = 7
 }
--- MiliastraUtility.Core/Serialization/ProtoSerializer.cs
+++ MiliastraUtility.Core/Serialization/ProtoSerializer.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Frozen;
 using System.Reflection;
-using System.Text.Json;
 
 namespace MiliastraUtility.Core.Serialization;
 
 public static class ProtoSerializer
 {
-    private static FrozenDictionary<int, PropertyInfo> GetProtoFields(Type type)
+    /// <summary>
+    /// 取得字段编号和属性信息的映射字典
+    /// </summary>
+    private static FrozenDictionary<int, PropertyInfo> GetFieldMap(Type type)
     {
         var result = new Dictionary<int, PropertyInfo>();
         var properties = from p in type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
@@ -17,20 +19,22 @@ public static class ProtoSerializer
         foreach (var p in properties)
         {
             var attribute = p.GetCustomAttribute<ProtoFieldAttribute>()!;
-            if (attribute.WrapperId != 0) result.Add(attribute.WrapperId, p);
-            else result.Add(attribute.Id, p);
+            result.Add(attribute.Id, p);
         }
 
         return result.ToFrozenDictionary();
     }
 
+    /// <summary>
+    /// 取得集合元素的类型
+    /// </summary>
     private static Type? GetElementType(Type type)
         =>  type.IsArray ? type.GetElementType()! : 
             type.IsGenericType ? type.GetGenericArguments()[0] : null;
 
-    private static bool IsEnumerableType(Type type)
-        => type != typeof(string) && typeof(IEnumerable).IsAssignableFrom(type);
-
+    /// <summary>
+    /// 尝试将值插入列表中，若列表为 null 会先进行初始化
+    /// </summary>
     private static void AddToList(PropertyInfo prop, object obj, object? element)
     {
         var list = prop.GetValue(obj) as IList;
@@ -44,80 +48,144 @@ public static class ProtoSerializer
         list.Add(element);
     }
 
-    public static T? Deserialize<T>(ref BufferReader reader) where T : notnull, new()
-        => (T?) Deserialize(typeof(T), ref reader);
-
-    private static object? Deserialize(Type type, ref BufferReader reader)
+    /// <summary>
+    /// 尝试解析数值，将根据 <see cref="ValueKind"/> 的值选择不同的解析分支
+    /// </summary>
+    private static object? ParseValue(Type type, ref BufferReader reader, ProtoTag tag, ValueKind kind)
     {
-        var fields = GetProtoFields(type);
-        object obj = Activator.CreateInstance(type)!;
-        while (reader.Available())
+        switch (kind)
+        {
+            case ValueKind.Null:
+                if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
+                tag.Consume(ref reader);
+                break;
+
+            case ValueKind.Varint:
+                if (tag.Type != WireType.VARINT) throw new InvalidDataException();
+                return Varint.Deserialize(ref reader).GetValue(type);
+
+            case ValueKind.Fixed32:
+                if (tag.Type != WireType.FIXED32) throw new InvalidDataException();
+                if (type == typeof(int)) return reader.ReadInt32LE();
+                if (type == typeof(uint)) return reader.ReadUInt32LE();
+                if (type == typeof(float)) return reader.ReadFloat();
+                throw new InvalidOperationException();
+
+            case ValueKind.Fixed64:
+                if (tag.Type != WireType.FIXED64) throw new InvalidDataException();
+                if (type == typeof(long)) return reader.ReadInt64LE();
+                if (type == typeof(ulong)) return reader.ReadUInt64LE();
+                if (type == typeof(double)) return reader.ReadDouble();
+                throw new InvalidOperationException();
+
+            case ValueKind.String:
+                if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
+                return reader.ReadString();
+
+            case ValueKind.Object:
+                if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
+                return Deserialize(type, ref reader, Varint.Deserialize<int>(ref reader));
+            
+            case ValueKind.List:
+                if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
+                if (!typeof(IList).IsAssignableFrom(type)) throw new InvalidOperationException();
+                return Deserialize(GetElementType(type)!, ref reader, Varint.Deserialize<int>(ref reader));
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// 拆除包装器，返回内部目标字段所属的标签和跳出包装器后的位置，并使读取器指向目标数据等待进一步解析
+    /// </summary>
+    private static (ProtoTag?, int) Unwrap(ref BufferReader reader, WrappedFieldAttribute attr)
+    {
+        int len = Varint.Deserialize<int>(ref reader);
+        int ret = reader.Position + len; // 保存跳出该包装器后的位置
+        foreach (int level in attr.Levels) // 找到位于最深处的目标字段
+        {
+            int end = reader.Position + len;
+            while (reader.Position < end)
+            {
+                ProtoTag tag = Varint.Deserialize(ref reader);
+                if (tag.Id == level)
+                {
+                    if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
+                    len = Varint.Deserialize<int>(ref reader);
+                    break;
+                }
+                tag.Consume(ref reader);
+            }
+        }
+        while (reader.Position < ret)
         {
             ProtoTag tag = Varint.Deserialize(ref reader);
-            if (!fields.TryGetValue(tag.Id, out PropertyInfo? prop) || !prop.CanWrite)
+            if (tag.Id == attr.Id) return (tag, ret);
+            tag.Consume(ref reader);
+        }
+        return (null, ret);
+    }
+
+    /// <summary>
+    /// 从读取器反序列化成指定类型的实例
+    /// </summary>
+    /// <typeparam name="T">目标类型</typeparam>
+    /// <param name="reader">读取器</param>
+    /// <exception cref="InvalidDataException"/>
+    /// <exception cref="InvalidOperationException"/>
+    public static T? Deserialize<T>(ref BufferReader reader) where T : notnull, new()
+        => (T?) Deserialize(typeof(T), ref reader, reader.Length);
+
+    /// <summary>
+    /// 从读取器反序列化成指定类型的实例，接受一个消息长度参数
+    /// </summary>
+    /// <typeparam name="T">目标类型</typeparam>
+    /// <param name="reader">读取器</param>
+    /// <param name="length">消息长度</param>
+    /// <exception cref="InvalidDataException"/>
+    /// <exception cref="InvalidOperationException"/>
+    public static T? Deserialize<T>(ref BufferReader reader, int length) where T : notnull, new()
+        => (T?)Deserialize(typeof(T), ref reader, length);
+
+    /// <summary>
+    /// 从读取器反序列化成指定类型的实例，接受一个消息长度参数
+    /// </summary>
+    /// <param name="type">目标类型</param>
+    /// <param name="reader">读取器</param>
+    /// <param name="length">消息长度</param>
+    /// <exception cref="InvalidDataException"/>
+    /// <exception cref="InvalidOperationException"/>
+    public static object? Deserialize(Type type, ref BufferReader reader, int length)
+    {
+        var map = GetFieldMap(type);
+        int end = reader.Position + length;
+        object obj = Activator.CreateInstance(type)!;
+        while (reader.Position < end)
+        {
+            ProtoTag tag = Varint.Deserialize(ref reader);
+            if (!map.TryGetValue(tag.Id, out PropertyInfo? prop) || !prop.CanWrite)
             {
-                tag.Consume(ref reader);
+                tag.Consume(ref reader); // 跳过未知或不可写入的属性
                 continue;
             }
 
             object? value = null;
             var attr = prop.GetCustomAttribute<ProtoFieldAttribute>()!;
-            switch (attr.Kind)
+            if (attr.Kind == ValueKind.Wrapped)
             {
-                case ValueKind.Varint:
-                {
-                    if (tag.Type != WireType.VARINT) throw new InvalidDataException();
-                    value = Varint.Deserialize(ref reader).GetValue(prop.PropertyType);
-                    prop.SetValue(obj, value);
-                    break;
-                }
-                case ValueKind.Fixed32:
-                {
-                    if (tag.Type != WireType.FIXED32) throw new InvalidDataException();
-                    if (prop.PropertyType == typeof(int)) value = reader.ReadInt32LE();
-                    if (prop.PropertyType == typeof(uint)) value = reader.ReadUInt32LE();
-                    if (prop.PropertyType == typeof(float)) value = reader.ReadFloat();
-                    prop.SetValue(obj, value);
-                    break;
-                }
-                case ValueKind.Fixed64:
-                {
-                    if (tag.Type != WireType.FIXED64) throw new InvalidDataException();
-                    if (prop.PropertyType == typeof(long)) value = reader.ReadInt64LE();
-                    if (prop.PropertyType == typeof(ulong)) value = reader.ReadUInt64LE();
-                    if (prop.PropertyType == typeof(double)) value = reader.ReadDouble();
-                    prop.SetValue(obj, value);
-                    break;
-                }
-                case ValueKind.String:
-                {
-                    if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
-                    value = reader.ReadString();
-                    prop.SetValue(obj, value);
-                    break;
-                }
-                case ValueKind.Object:
-                {
-                    if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
-                    value = Deserialize(prop.PropertyType, ref reader);
-                    prop.SetValue(obj, value);
-                    break;
-                }
-                case ValueKind.List:
-                {
-                    if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
-                    if (!IsEnumerableType(prop.PropertyType)) throw new NotSupportedException();
-                    Type elementType = GetElementType(prop.PropertyType)!;
-                    AddToList(prop, obj, Deserialize(elementType, ref reader));
-                    break;
-                }
-                case ValueKind.Null:
-                {
-                    if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
-                    tag.Consume(ref reader);
-                    break;
-                }
+                if (tag.Type != WireType.LENGTH) throw new InvalidDataException();
+                var wrapperInfo = prop.GetCustomAttribute<WrappedFieldAttribute>() ??
+                    throw new InvalidOperationException("缺少修饰特性 WrappedFieldAttribute");
+
+                var (target, ret) = Unwrap(ref reader, wrapperInfo);
+                if (target != null)
+                    value = ParseValue(prop.PropertyType, ref reader, (ProtoTag)target, wrapperInfo.Kind);
+                reader.Seek(ret, SeekOrigin.Begin);
             }
+            else value = ParseValue(prop.PropertyType, ref reader, tag, attr.Kind);
+
+            if (value is null) continue;
+            if (attr.Kind == ValueKind.List) AddToList(prop, obj, value);
+            else prop.SetValue(obj, value);
         }
         return obj;
     }
